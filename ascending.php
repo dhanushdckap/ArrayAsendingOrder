ef12989bb492fb60addad143a8580e79cc667321
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-05-15 17:02:54 +0530

    I have some changed

--- ascending.php
+++ ascending.php
@@ -2,19 +2,22 @@
 
 
    
-$input = [1,2,5];
+$input = [4,5,1,2];
 $result = [];
 
-for($i=0;$i < count($input);$i++){
+for($i=0;$i < count($input)-1;$i++){
     if($input[$i] < $input[$i+1]){
         $new = $input[$i];
         $input[$i] = $input[$i+1];
         $input[$i+1] = $new;
-        $result[] = $input; 
+        $result[] = $input;
         print_r(json_encode($input));
         echo "\n";
     }
 }
+
+
+
   
   
 
