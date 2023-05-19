c24a5dbdb3452295bbe7959a3db19a0dba8dad2f
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-05-19 15:43:14 +0530

    completed

--- ascending.php
+++ ascending.php
@@ -1,35 +1,35 @@
 <?php
 
-// $input = [10,2,5,12,34];
-// $result=[];
+$input = [10,2,5,12,34];
+$result=[];
 
-// function recursion($input)
-// {
+function recursion($input)
+{
 
-//     $number = count($input);
+    $number = count($input);
 
-//     if($number > 5){
-//         echo "Please enter the 5 numbers in the array";
-//         echo "\n";
-//     }
-//     else
-//     {
-//         for($i = 0; $i < $number - 1; $i++){
-//             for($j = 0;$j < $number - $i - 1;$j++){
-//                 if ($input[$j] < $input[$j + 1]) {
-//                     $temporary = $input[$j];
-//                     $input[$j] = $input[$j + 1];
-//                     $input[$j + 1] = $temporary;
-//                     $result[] = $input;
-//                     print_r(json_encode($input));
-//                     echo "\n";
-//                 }
-//             }
-//         }
-//     }
-// }
+    if($number > 5){
+        echo "Please enter the 5 numbers in the array";
+        echo "\n";
+    }
+    else
+    {
+        for($i = 0; $i < $number - 1; $i++){
+            for($j = 0;$j < $number - $i - 1;$j++){
+                if ($input[$j] < $input[$j + 1]) {
+                    $temporary = $input[$j];
+                    $input[$j] = $input[$j + 1];
+                    $input[$j + 1] = $temporary;
+                    $result[] = $input;
+                    print_r(json_encode($input));
+                    echo "\n";
+                }
+            }
+        }
+    }
+}
 
-// echo recursion($input);
+echo recursion($input);
 
 
 
